2c3458be9454c811981e18cab89168a513336c65
angelsteger <angel.steger@gmail.com>
2015-08-16 22:58:59 -0700

    Codepath application app
    Super basic app with color, icon, and the ability to split the bill for
    parties up to 5.

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var twoLabel: UILabel!
+    @IBOutlet weak var threeLabel: UILabel!
+    @IBOutlet weak var fourLabel: UILabel!
+    @IBOutlet weak var fiveLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        var two = total / 2
+        var three = total / 3
+        var four = total / 4
+        var five = total / 5
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        twoLabel.text = "$\(two)"
+        threeLabel.text = "$\(three)"
+        fourLabel.text = "$\(four)"
+        fiveLabel.text = "$\(five)"
+        
+        twoLabel.text = String(format: "$%.2f", two)
+        threeLabel.text = String(format: "$%.2f", three)
+        fourLabel.text = String(format: "$%.2f", four)
+        fiveLabel.text = String(format: "$%.2f", five)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
